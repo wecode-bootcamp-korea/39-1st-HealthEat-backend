32477f26caaa115c569ff28e88cf8211d87227a5
Joe <swjo1229@gmail.com>
2022-11-15 14:37:42 +0900

    초기 환경 세팅(수정)

--- app.js
+++ app.js
@@ -3,21 +3,15 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 
+const { sqlDataSource } = require("./src/models/data-source");
+const { router } = require("./src/routes");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-const { DataSource } = require("typeorm");
-const sqlDataSource = new DataSource({
-  type: process.env.TYPEORM_CONNECTION,
-  host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-});
+app.use(router);
 
 const PORT = 3000;
 
